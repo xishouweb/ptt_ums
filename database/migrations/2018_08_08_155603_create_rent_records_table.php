dd0d052acb00dd85d0c9e93630879385edb2f0ff
erdangjia <tianlong.zhang@bangtuike.com>
2018-08-09 14:54:29 +0800

    create table for super agency

---
+++ database/migrations/2018_08_08_155603_create_rent_records_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRentRecordsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rent_records', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->string('team_id', 20);
+            $table->decimal('amount', 10, 2);
+            $table->string('action');
+            $table->tinyInteger('status')->default(0);
+            $table->decimal('settlement_amount', 10, 2)->nullable();
+            $table->integer('campaign_id')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rent_records');
+    }
+}
