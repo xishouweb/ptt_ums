35a4bd8f3b489d912d5914a1880193314c71af8b
kana <tailei.yang@bangtuike.com>
2018-08-21 18:59:45 +0800

    laravel-admin生成

---
+++ app/Admin/routes.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Routing\Router;
+use Illuminate\Support\Facades\Route;
+use Encore\Admin\Facades\Admin;
+
+Admin::registerAuthRoutes();
+
+Route::group([
+    'prefix'        => config('admin.route.prefix'),
+    'namespace'     => config('admin.route.namespace'),
+    'middleware'    => config('admin.route.middleware'),
+], function (Router $router) {
+    $router->get('/', 'HomeController@index');
+    $router->get('/wallet/notice', 'NoticeController@index');
+});
