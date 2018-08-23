0f04fc220273997ea4821d283fc8d8921373da73
kana <tailei.yang@bangtuike.com>
2018-08-23 15:49:30 +0800

    配置alioss，filesystems

--- config/alioss.php
+++ config/alioss.php
@@ -6,14 +6,25 @@
  * Time: 下午6:02
  */
 
+/**
+ * ossServer            外网
+ * ossServerInternal    内网
+ * accessKeyId          key
+ * accessKeySecret      secret
+ * bucketName           bucket
+ * city                 city
+ * networkType          network
+ * isInternal           is internal
+ * ossURL               oss url
+ */
 return [
-    'ossServer' => env('OSS_SERVER', null),                      // 外网
-    'ossServerInternal' => env('OSS_SERVER_INTERNAL', null),      // 内网
-    'accessKeyId' => env('OSS_KEY_ID', null),                     // key
-    'accessKeySecret' => env('OSS_KEY_SECRET', null),             // secret
-    'bucketName' => env('OSS_BUCKET_NAME', null),                  // bucket
-    'city' => env('OSS_CITY', null),                  // city
-    'networkType' => env('OSS_NETWORK_TYPE', null),                  // network
-    'isInternal' => env('OSS_IS_INTERNAL', false),                  // is internal
-    'ossURL' => env('OSS_URL', false),                  //  oss url
+    'ossServer' => env('OSS_SERVER', 'http://oss-cn-hangzhou.aliyuncs.com'),
+    'ossServerInternal' => env('OSS_SERVER_INTERNAL', 'http://oss-cn-hangzhou-internal.aliyuncs.com'),
+    'accessKeyId' => env('OSS_KEY_ID', 'PNT702y47BBk78d6'),
+    'accessKeySecret' => env('OSS_KEY_SECRET', 'Ta6sUmuR7byCnpPuyEaugzPO7zGguj'),
+    'bucketName' => env('OSS_BUCKET_NAME', 'btkverifiedfiles'),
+    'city' => env('OSS_CITY', '杭州'),
+    'networkType' => env('OSS_NETWORK_TYPE', '经典网络'),
+    'isInternal' => env('OSS_IS_INTERNAL', false),
+    'ossURL' => env('OSS_URL', 'http://btkverifiedfiles.oss-cn-hangzhou.aliyuncs.com'),
 ];
