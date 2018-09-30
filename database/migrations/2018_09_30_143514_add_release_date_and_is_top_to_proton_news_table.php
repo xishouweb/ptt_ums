ddc6ebbac31cfd4847bc2e5223c65987122dace0
erdangjia <tianlong.zhang@bangtuike.com>
2018-09-30 15:07:02 +0800

    fix proton site list api and admin

---
+++ database/migrations/2018_09_30_143514_add_release_date_and_is_top_to_proton_news_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddReleaseDateAndIsTopToProtonNewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('proton_news', function (Blueprint $table) {
+            $table->timestamp('release_date')->default(date('Y-m-d H:i:s'));
+            $table->tinyInteger('is_top')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('proton_news', function (Blueprint $table) {
+            $table->dropColumn('release_date');
+            $table->dropColumn('is_top');
+        });
+    }
+}
