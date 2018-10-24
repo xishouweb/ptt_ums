3fe3c02379558d9e9765c2fee470413892d83d40
kana <tailei.yang@bangtuike.com>
2018-10-24 19:07:53 +0800

    验证码接口，验证码登录注册接口，密码登录接口，用户详情接口

--- app/Http/Controllers/App/UserController.php
+++ app/Http/Controllers/App/UserController.php
@@ -40,18 +40,33 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $phone = $request->input('phone');
+        $country = $request->input('country');
+
+        $user = User::create([
+            'phone'      => $phone,
+            'password'   => Hash::make($phone),
+            'update_key' => md5($phone . env('APP_KEY')),
+            'type'       => 'wallet',
+            'country'    => $country,
+        ]);
+
+        return $user;
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show()
     {
-        //
+        $user = Auth::user();
+        $data['id'] = $user->id;
+        $data['phone'] = $user->phone;
+        $data['nickname'] = $user->nickname ?: 'User_' . md5($user->phone);
+        $data['avatar'] = $user->avatar ?: 'http://btkverifiedfiles.oss-cn-hangzhou.aliyuncs.com/photos/2017_08_21_14_48_05_1_2933.png';
+        return response()->json($data);
     }
 
     /**
@@ -74,7 +89,19 @@ class UserController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $user = Auth::user();
+        if ($request->input('nickname')) {
+            $user->nickname = $request->input('nickname');
+        }
+        if ($request->file('avatar')) {
+            $photo = Photo::upload($request, 'avatar');
+            if (!$photo) {
+                return response()->json(['message' => '图片上传失败'], 500);
+            }
+            $user->avatar = $photo->url;
+        }
+        $user->save();
+        return response()->json(['message' => '修改成功']);
     }
 
     /**
@@ -88,72 +115,47 @@ class UserController extends Controller
         //
     }
 
+    //密码登录
     public function login(Request $request)
     {
-        $phone = $request->input('phone');
-        $country = $request->input('country');
-        $captcha = $request->input('captcha');
+        if (!$request->input('phone') || !$request->input('country') || !$request->input('password')) {
+            return response()->json(['message' => '手机号，密码和国家区号均不能为空'], 401);
+        }
 
-        if (!$phone || !$country) {
-            return response()->json(['message' => '手机号和国家区号不能为空'], 401);
+        $result = Auth::attempt(['phone' => $request->input('phone'), 'password' => $request->input('password')]);
+        if (!$result) {
+            return response()->json(['message' => '账户不存在或密码错误'], 403);
+        }
+
+        $user = Auth::user();
+        $content['token'] = 'Bearer ' . $user->createToken('Wallet')->accessToken;
+        $content['message'] = '登录成功';
+        return response()->json($content);
+    }
+
+    //验证码登录
+    public function fastLogin(Request $request)
+    {
+        if (!$request->input('phone') || !$request->input('country') || !$request->input('captcha')) {
+            return response()->json(['message' => '手机号，验证码和国家区号均不能为空'], 401);
         }
         //验证码
-        $c_result = Captcha::pre_valid($phone, $captcha);
+        $c_result = Captcha::valid($request->input('phone'), $request->input('captcha'));
         if (!$c_result) {
-            return response()->json(['message' => '验证码错误或过期'], 401);
+            return response()->json(['message' => '验证码不存在或过期'], 401);
         }
 
         //判断用户是否存在
-        $user = User::where('phone', $phone)->first();
+        $content['is_new'] = false;
+        $user = User::where('phone', $request->input('phone'))->first();
         if (!$user) {
-            $user = User::create([
-                'phone'      => $phone,
-                'password'   => Hash::make($phone),
-                'update_key' => md5($phone . env('APP_KEY')),
-                'type'       => 'wallet',
-                'country'    => $country,
-            ]);
+            $user = $this->store($request);
+            $content['is_new'] = true;
         }
 
         $content['token'] = 'Bearer ' . $user->createToken('Wallet')->accessToken;
         $content['message'] = '登录成功';
 
         return response()->json($content);
-    }
-
-    public function detail()
-    {
-        $user = Auth::user();
-        $data['phone'] = $user->phone;
-        $data['nickname'] = $user->nickname ?: 'User_' . md5($user->phone);
-        $data['avatar'] = $user->avatar ?: 'http://btkverifiedfiles.oss-cn-hangzhou.aliyuncs.com/photos/2017_08_21_14_48_05_1_2933.png';
-        return response()->json($data);
-    }
-
-    public function updateNickname(Request $request)
-    {
-        if (!$request->input('nickname')) {
-            return response()->json(['message' => '昵称不能为空'], 401);
-        }
-        $user = Auth::user();
-        $user->nickname = $request->input('nickname');
-        $user->save();
-        return response()->json(['message' => '修改成功']);
-    }
-
-    //todo
-    public function updateAvatar(Request $request)
-    {
-        if (!$request->input('avatar')) {
-            return response()->json(['message' => '请上传头像'], 401);
-        }
-        $photo = Photo::upload($request, 'avatar');
-        if (!$photo) {
-            return $this->apiResponse([], '图片上传失败!', 1);
-        }
-        $user = Auth::user();
-        $user->avatar = $photo->url;
-        $user->save();
-        return response()->json(['message' => '上传成功']);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -21,14 +21,18 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::prefix('app')->group(function () {
     Route::namespace('App')->group(function (Router $router) {
+        $router->get('/announcement', 'AnnouncementController@index');
         $router->get('/banner', 'BannerController@index');
         $router->get('/notice', 'NoticeController@index');
+        $router->post('/user/fast_login', 'UserController@fastLogin');
         $router->post('/user/login', 'UserController@login');
+        $router->get('/captcha', 'ToolController@getCaptcha');
         $router->get('/price', 'ToolController@getPrice');
         $router->get('/search_token', 'ToolController@searchToken');
-        $router->get('/announcement', 'AnnouncementController@index');
+
         Route::group(['middleware' => 'auth:api'], function(Router $router) {
-            $router->get('/user/detail', 'UserController@detail');
+            $router->get('/user', 'UserController@show');
+            $router->put('/user', 'UserController@update');
         });
     });
 });
