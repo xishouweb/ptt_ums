6995de65ebcbe6bddf652c60e03ee8b6827cebb8
erdangjia <tianlong.zhang@bangtuike.com>
2018-10-30 15:11:12 +0800

    fix proton news bug

--- app/Models/ProtonNew.php
+++ app/Models/ProtonNew.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class ProtonNew extends Model
+class ProtonNew extends BaseModel implements FormatInterface
 {
     use SoftDeletes;
 
@@ -14,4 +14,18 @@ class ProtonNew extends Model
     const IS_TOP_YES = 1;
 
     protected $guarded = ['id'];
+
+    public function format($source = [])
+    {
+        $data['title'] = $this->title;
+        $data['description'] = $this->description;
+        $data['img_base'] = $this->img_base;
+        $data['img'] = $this->img;
+        $data['url'] = $this->url;
+        $data['release_date'] = $this->release;
+
+
+        return $data;
+
+    }
 }
