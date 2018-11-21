a70e4f91051afe61b378de0ba78e9d9b14d9576b
erdangjia <m.dangjia@gmail.com>
2018-11-21 15:21:06 +0800

    wechat auth

---
+++ app/Http/Middleware/WxSuperCampaign.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use EasyWeChat\Factory;
+use Illuminate\Support\Facades\Session;
+
+class WxSuperCampaign
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+
+        $wechat = Session::get('super_campaign');
+
+        if (!$wechat) {
+
+            $auth = $this->__get_auth();
+            $user = $auth->authorize(null, 'snsapi_userinfo');
+
+            if ($user['unionid']) {
+                $wechat = WechatUser::createOrUpdate($user, 'unionid', 'yiqi', request()->get('user_id', null));
+                Session::put('wechatUser', $wechat);
+            } else {
+                $openid        = $user['openid'];
+                $wechat_openid = WechatOpenid::where('openid', $openid)->first();
+                if (!$wechat_openid) {
+                    $user = $auth->redirect();
+                    Session::put('wechatUser', $user->all());
+                }
+
+                $wechat = WechatUser::where('unionid', $wechat_openid->unionid)->first();
+                if ($wechat) {
+                    Session::put('wechatUser', $wechat);
+                } else {
+                    $user = $auth->redirect();
+                    Session::put('wechatUser', $user->all());
+                }
+            }
+        }
+        return $next($request);
+    }
+
+    private function __get_auth()
+    {
+//        return new Au(config('wechat.app_id_yiqi'), config('wechat.secret_yiqi'));
+        return Factory::officialAccount();
+    }
+}
