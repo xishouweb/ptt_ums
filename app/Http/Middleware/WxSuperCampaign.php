36efacfc14534687902249a331306934eda1412d
erdangjia <m.dangjia@gmail.com>
2018-11-22 14:38:51 +0800

    fix bug

--- app/Http/Middleware/WxSuperCampaign.php
+++ app/Http/Middleware/WxSuperCampaign.php
@@ -18,36 +18,13 @@ class WxSuperCampaign
     public function handle($request, Closure $next)
     {
 
-        $wechat = Session::get('super_campaign');
+        $session = Session::get('wechat.oauth_user.default');
 
-        if (!$wechat) {
-
-            $auth = Factory::officialAccount(config('wechat.official_account.default'));
-            $user = $auth->oauth->scopes(['snsapi_userinfo'])
-                ->redirect();
-
-            \Log::info('user', [$user]);
-//            if ($user['unionid']) {
-//                $wechat = WechatUser::createOrUpdate($user, 'unionid', 'yiqi', request()->get('user_id', null));
-//                Session::put('wechatUser', $wechat);
-//            } else {
-//                $openid        = $user['openid'];
-//                $wechat_openid = WechatOpenid::where('openid', $openid)->first();
-//                if (!$wechat_openid) {
-//                    $user = $auth->redirect();
-//                    Session::put('wechatUser', $user->all());
-//                }
-//
-//                $wechat = WechatUser::where('unionid', $wechat_openid->unionid)->first();
-//                if ($wechat) {
-//                    Session::put('wechatUser', $wechat);
-//                } else {
-//                    $user = $auth->redirect();
-//                    Session::put('wechatUser', $user->all());
-//                }
-//            }
-//        }
-            return $next($request);
+        if ($session) {
+            \Log::info('session = ', [$session]);
         }
+
+
+        return $next($request);
     }
 }
