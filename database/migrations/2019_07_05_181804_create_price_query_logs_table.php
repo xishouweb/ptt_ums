6e27da71be025d8129c0231e4db59be6f986f24b
erdangjia <m.dangjia@gmail.com>
2019-07-05 19:15:34 +0800

    行业助手 价格查询统计 建表

---
+++ database/migrations/2019_07_05_181804_create_price_query_logs_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePriceQueryLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('price_query_logs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('user_id')->nullable();
+            $table->string('xu_host_id');
+            $table->string('xu_group_id');
+            $table->string('xu_robot_id');
+            $table->string('symbol', 16);
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->index(['xu_host_id', 'xu_group_id', 'user_id', 'xu_robot_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('price_query_logs');
+    }
+}
