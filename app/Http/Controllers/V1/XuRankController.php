bb396f1b695c0713c28a8ba1dff91f7e1cd0d585
erdangjia <m.dangjia@gmail.com>
2019-07-10 14:32:24 +0800

    币价查询统计排行榜 微信授权

---
+++ app/Http/Controllers/V1/XuRankController.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by sublime.
+ * User: erdangjia
+ * Date: 2019/7/12
+ * Time: 10:41
+ */
+
+namespace App\Http\Controllers\V1;
+
+use App\Http\Controllers\Controller;
+
+class XuRankController extends Controller
+{
+    public function test()
+    {
+        $user = session('wechat.oauth_user.default'); // 拿到授权用户资料
+
+        dd($user);
+    }
+}
