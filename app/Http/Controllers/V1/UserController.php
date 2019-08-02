a57a8a83ee6bf163d27b6a11359b84388f4cfffb
erdangjia <m.dangjia@gmail.com>
2019-08-02 17:01:46 +0800

    币价查询机器人领养跳转

--- app/Http/Controllers/V1/UserController.php
+++ app/Http/Controllers/V1/UserController.php
@@ -7,5 +7,8 @@ use App\Http\Controllers\Controller;
 
 class UserController extends Controller
 {
-    //
+    public function redirectXu()
+    {
+        header("Location: http://www.baidu.com");
+    }
 }
