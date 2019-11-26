18e3b218e58d8e847348e7d072192afb48ec04ed
kana <tailei.yang@bangtuike.com>
2019-11-26 15:06:29 +0800

    修改dataRecordController返回

--- app/Http/Controllers/Business/DataRecordController.php
+++ app/Http/Controllers/Business/DataRecordController.php
@@ -27,7 +27,7 @@ class DataRecordController extends Controller
 		$items = TrackItem::join('user_applications', 'track_items.user_application_id', '=', 'user_applications.id')
             ->where('track_items.user_id', $user->id)
             ->orderBy('track_items.id', 'desc')
-            ->select('track_items.id', 'track_items.hx', 'track_items.created_at', 'track_items.bc_id', 'user_applications.name')
+            ->select('track_items.id', 'track_items.hx', 'track_items.created_at', 'track_items.bc_id', 'track_items.type', 'user_applications.name')
             ->paginate(10);
 		return response()->json(['data' => $items]);
 	}
