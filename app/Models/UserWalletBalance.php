d38ba97c1d3e81ba6ffec6b3d32ad68b936699ff
kana <tailei.yang@bangtuike.com>
2019-12-10 17:40:56 +0800

    云钱包接口

--- app/Models/UserWalletBalance.php
+++ app/Models/UserWalletBalance.php
@@ -5,10 +5,12 @@ namespace App\Models;
 use App\User;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class UserWalletBalance extends Model
 {
-    //
+    use SoftDeletes;
+
     public function users()
     {
         return $this->belongsTo(User::class, 'user_id', 'id');
