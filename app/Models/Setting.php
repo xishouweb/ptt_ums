8dfa4ca3591f46a52979e7a5c2ca36cd113c83a8
kana <tailei.yang@bangtuike.com>
2020-01-03 17:57:53 +0800

    setting

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Setting extends Model
 {
+    protected $fillable = ['key', 'value'];
+
     public static function retrieve($key, $default = null, $createIfNotExist = false)
     {
         $model = self::where('key', $key)->first();
