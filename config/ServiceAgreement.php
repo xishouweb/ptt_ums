f06a2c8a800bcd6b9ad62a096d9717a61f325f41
kana <tailei.yang@bangtuike.com>
2020-01-03 14:33:41 +0800

    用户协议

---
+++ config/ServiceAgreement.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kana
+ * Date: 2019/12/25
+ * Time: 10:50
+ */
+
+return [
+    'cn' => [
+        'title' => '用户协议',
+        'content' => "用户协议"
+    ],
+    'en' => [
+        'title' => '用户协议',
+        'content' => "用户协议"
+    ],
+];
